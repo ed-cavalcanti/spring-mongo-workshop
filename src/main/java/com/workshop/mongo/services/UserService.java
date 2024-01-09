78697a6ab3e7490a2d34762124929061e1272ea4
ed-cavalcanti <edney2809@gmail.com>
2024-01-09 14:20:30 -0300

    [feat] User delete

--- src/main/java/com/workshop/mongo/services/UserService.java
+++ src/main/java/com/workshop/mongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
         return repository.insert(newUser);
     }
 
+    public void delete(String id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
     public User fromDTO(UserDTO userDto) {
         return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
