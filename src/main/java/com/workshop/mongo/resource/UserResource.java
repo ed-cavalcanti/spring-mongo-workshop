084bdee744e34a2cbf08085971269cd18db3f035
ed-cavalcanti <edney2809@gmail.com>
2024-01-09 13:27:12 -0300

    [refact] return UserDTO

--- src/main/java/com/workshop/mongo/resource/UserResource.java
+++ src/main/java/com/workshop/mongo/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.workshop.mongo.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.workshop.mongo.domain.User;
+import com.workshop.mongo.dto.UserDTO;
 import com.workshop.mongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-    
+
     @Autowired
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
