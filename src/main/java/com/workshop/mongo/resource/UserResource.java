b2cd596f007310487c9fdd5c2981dee9f36c61e9
ed-cavalcanti <edney2809@gmail.com>
2024-01-09 15:20:15 -0300

    [feat] findPosts by user id

--- src/main/java/com/workshop/mongo/resource/UserResource.java
+++ src/main/java/com/workshop/mongo/resource/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.workshop.mongo.domain.Post;
 import com.workshop.mongo.domain.User;
 import com.workshop.mongo.dto.UserDTO;
 import com.workshop.mongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
         user.setId(id);
         user = service.update(user);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 }
